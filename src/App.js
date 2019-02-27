e0a4630888beec4da61d4ff396b736f0fbffd745
Ummer Khan <ummernkhangit@gmail.com>
2019-02-27 09:33:35 -0800

    User's List fetch request added

--- src/App.js
+++ src/App.js
@@ -3,23 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    id: '',
+    username: '',
+    email: '',
+    password: '',
+    verify_password: '',
+    is_admin: '',
+  }
+
+  async componentDidMount(){
+    this.getUsers()
+  }
+
+  getUsers = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/api/v1/users', {
+        credentials: 'include'
+      })
+      if (response.ok) {
+        const responseParsed = await response.json()
+        console.log(responseParsed)
+      }
+    } catch(err) {
+        console.log(err)
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
